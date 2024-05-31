989c31b0c177995db63c624be0ef4b1ef7f3c0c8
josehenriquepg <hpereira012@gmail.com>
2024-05-30 23:02:51 -0300

    feat: User login and JWT token generation

--- controllers/userController.go
+++ controllers/userController.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"net/http"
+	"time"
 	"user-management-api/config"
 	"user-management-api/models"
 
@@ -13,12 +14,12 @@ import (
 var jwtKey = []byte("2RBF/4lD1wBMWxiCWjV4gB7aLB4Rg41Rv4S1+w6iixk=")
 
 type Credentials struct {
-	Username string `json: "username"`
-	Password string `json: "password"`
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
 
 type Claims struct {
-	Username string `json: "username"`
+	Username string `json:"username"`
 	jwt.StandardClaims
 }
 
@@ -44,6 +45,43 @@ func Register(c *gin.Context) {
 		return
 	}
 
-	c.JSON((http.StatusCreated, gin.H{"message": "User registered sucessfully"}))
+	c.JSON(http.StatusCreated, gin.H{"message": "User registered sucessfully"})
 }
 
+// User login and JWT token generation
+func Login(c *gin.Context) {
+	var creds Credentials
+	if err := c.BindJSON(&creds); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request"})
+		return
+	}
+
+	var user models.User
+	db := config.GetDB()
+	if err := db.Where("username = ?", creds.Username).First(&user).Error; err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "User not found"})
+		return
+	}
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(creds.Password)); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid password"})
+		return
+	}
+
+	expirationTime := time.Now().Add(24 * time.Hour)
+	claims := &Claims{
+		Username: creds.Username,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: expirationTime.Unix(),
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodES256, claims)
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Error generation token"})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
+}
