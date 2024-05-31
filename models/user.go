161c5ea11ddfcec1913840104acf2cff47e9d5a7
josehenriquepg <hpereira012@gmail.com>
2024-05-30 23:04:14 -0300

    fix: gorm format tag

--- models/user.go
+++ models/user.go
@@ -5,7 +5,7 @@ import "github.com/jinzhu/gorm"
 type User struct {
 	gorm.Model
 	Username string `gorm:"unique;not null"`
-	Password string `gorm: "not null"`
+	Password string `gorm:"not null"`
 }
 
 func (u *User) SaveUser(db *gorm.DB) (*User, error) {
